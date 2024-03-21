3a38cc255f9cb82cc12217af2cbb1cb4b99fe141
ROG STRIX <s160421034@student.ubaya.ac.id>
2024-03-21 12:36:50 +0700

    Week4

---
+++ app/src/main/java/com/ubayadef/advweek4160421034/model/Model.kt
@@ -0,0 +1,9 @@
+package com.ubayadef.advweek4160421034.model
+
+data class Student(
+    var id:String?,
+    var name:String?,
+    var dob:String?,
+    var phone:String?,
+    var photoUrl:String?
+)
